951063e71a74cf1d308d9322e5a0d4852ac5f45a
David Blood <davidbjourno@users.noreply.github.com>
2016-12-21 18:10:17 +0000

    Initial

--- config/article.js
+++ config/article.js
@@ -5,48 +5,46 @@ export default () => ({ // eslint-disable-line
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/life-arts-qoty2016 get filled in by the ./configure script
-  url: 'https://ig.ft.com/sites/life-arts-qoty2016',
+  url: 'https://ig.ft.com/sites/quiz/life-arts-qoty2016',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2016-12-22T15:00:00Z'),
 
-  headline: 'Politics and the English Language',
+  headline: 'FT quiz of the year 2016',
 
   // summary === standfirst (Summary is what the content API calls it)
-  summary: 'Political language is designed to make lies sound truthful' +
-           'and murder respectable, and to give an appearance of solidity to pure wind',
+  summary: 'A year’s supply of Laurent-Perrier Cuvée Rosé awaits the winner of this cultural challenge',
 
   topic: {
-    name: 'Starter Kit',
-    url: '/foo',
+    name: 'Life & Arts',
+    url: 'https://www.ft.com/life-arts',
   },
 
   relatedArticle: {
-    text: 'Related article »',
-    url: 'https://en.wikipedia.org/wiki/Politics_and_the_English_Language',
+    text: '',
+    url: '',
   },
 
   mainImage: {
-    title: '',
-    description: '',
-    url: '',
-    width: 2048, // ensure correct width
-    height: 1152, // ensure correct height
+    title: 'FT quiz of the year 2016',
+    description: 'Montage of 2016 news images',
+    url: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Abc7218b4-c777-11e6-8f29-9445cac8966f?source=ig&width=700',
+    width: 700, // ensure correct width
+    height: 394, // ensure correct height
   },
 
   // Byline can by a plain string, markdown, or array of authors
   // if array of authors, url is optional
   byline: [
-    { name: 'Author One', url: '/foo/bar' },
-    { name: 'Author Two' },
+    { name: 'Ludovic Hunter-Tilney', url: 'https://www.ft.com/stream/authorsId/Q0ItTEgwMDMwMQ==-QXV0aG9ycw==' },
   ],
 
   // Appears in the HTML <title>
-  title: '',
+  title: 'FT quiz of the year 2016',
 
   // meta data
-  description: '',
+  description: 'FT quiz of the year 2016',
 
   /*
   TODO: Select Twitter card type -
