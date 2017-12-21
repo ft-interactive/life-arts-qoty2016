b0e54a8f7ca17941d6d4074461b136b38503ad00
David Blood <davidbjourno@users.noreply.github.com>
2017-12-21 14:38:29 +0000

    Enable onward journey (?)

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,5 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'list/graphics' },
-    { rows: 1, list: 'list/highlights' },
+    { rows: 2, list: 'thing/f967910f-67d5-31f7-a031-64f8af0d9cf1' },
   ],
 });
